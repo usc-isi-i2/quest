8ed1fad192e437c63a9f86c037bba480916f1111
Justin Cho <jcho@isi.edu>
2025-10-27 22:06:54 -0700

    hide

--- app.py
+++ app.py
@@ -294,8 +294,25 @@ def main():
     #GithubIcon {
         visibility: hidden;
     }
+    
+    #MainMenu {
+        visibility: hidden;
+    }
     </style>
     """, unsafe_allow_html=True)
+    
+    st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
     
     st.title("Question Quality Human Evaluation")
 
